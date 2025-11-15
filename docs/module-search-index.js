dae0c00d0467a3ef2b65a42e9a4fb79300becb0f
voiculescubogdan <voiculescubogdan22@stud.ase.ro>
2025-11-15 11:05:51 +0200

    added javadoc page and word

---
+++ docs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.example.proiect_tema4_javafx"}];updateSearchResults();
---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.proiect_tema4_javafx","l":"com.example.proiect_tema4_javafx"}];updateSearchResults();
